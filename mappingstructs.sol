c766fee82abcb8e54b5cbffa7730696c83a93259
SkynetDev <andrejohnson0017@gmail.com>
2020-12-24 22:39:44 -0500

    added a function to withdraw partial amounts and implemented the checks effects interation pattern

--- mappingstructs.sol
+++ mappingstructs.sol
@@ -4,6 +4,8 @@ contract mappingsStruct {
     
     address owner;
     
+    mapping(address => uint) public balanceReceived;
+    
     constructor() public {
         owner = msg.sender;
     }
@@ -13,12 +15,24 @@ contract mappingsStruct {
     }
     
     function sendMoney() public payable {
-        
+        balanceReceived[msg.sender] += msg.value;
     }
     
     function withdrawAllMoney(address payable _to) public {
         require(msg.sender == owner,"You are not the owner");
-        _to.transfer(this.getBalance());
+        _to.transfer(address(this).balance);
          
+    }
+    
+    function withdrawBalance(address payable _to) public {
+        uint balanceToSend = balanceReceived[msg.sender];
+        balanceReceived[msg.sender] = 0;
+        _to.transfer(balanceToSend);
+    }
+    
+    function withdrawMoney(address payable _to, uint _amount) public {
+        require(balanceReceived[msg.sender] >= _amount, "You don't have enough funds");
+        balanceReceived[msg.sender] -= _amount;
+        _to.transfer(_amount);
     }
 }
